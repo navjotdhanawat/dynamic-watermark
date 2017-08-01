3151b44ff1c41928fa4fe62f3884df17cb9ff498
Navjot Dhanawat <n.dhanawat@splendornet.com>
2017-08-01 19:17:20 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# dynamic-watermark
+# How to Setup: Dynamic Watermark to add watermark(image/text) over image using nodejs.
+
+```
+npm install dynamic-watermark --save
+
+```
+
+Dynamic Watermark is npm watermark module to add watermark over image. It can add image as well as text watermark on given positions
---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * dynamic-watermark
+ *
+ *
+ * Copyright (c) 2017 Navjot Dhanawat
+ * Licensed under the MIT license.
+ */
+
+/**
+ * Dynamic Watermark is npm watermark module to add watermark over image.
+ * It can add image as well as text watermark on given positions.
+ * @param  {options}
+ * @return {callback}
+ */
+
+
+
+var embed = function (data, callback) {
+    callback({ status: 1 });
+};
+
+module.exports.embed = embed;
