eeb4d173e4bb4f5db6cd8ccd1eba7c5033c6c32f
Navjot Dhanawat <n.dhanawat@splendornet.com>
2017-08-17 19:26:26 +0530

    Updated readme

--- README.md
+++ README.md
@@ -6,3 +6,30 @@ npm install dynamic-watermark --save
 ```
 
 Dynamic Watermark is npm watermark module to add watermark over image. It can add image as well as text watermark on given positions
+
+
+After successful installation of dynamic-watermark follow below steps:
+
+Step 1: include dynamic-watermark package:
+
+var watermark = require('dynamic-watermark');
+
+
+Step 2: Provide following options:
+
+var options = {
+    source : "source/file/path",
+    logo: "logo/file/path", // This is optional if you have provided text Watermark
+    destination: "destination/file/path",
+    position: "left-top",    // left-top, left-bottom, right-top, right-bottom
+    type: "text",   // text or image
+    text: "Watermark Text"  // This is optional if you have provided logo file
+}
+
+Step 3: Final step is to call embed method by passing above options.
+
+watermark.embed(options, function(status) {
+    //Do what you want to do here
+    console.log(status);
+})
+
--- index.js
+++ index.js
@@ -14,9 +14,79 @@
  */
 
 
+var gm = require('gm');
 
-var embed = function (data, callback) {
-    callback({ status: 1 });
+var embed = function (options, callback) {
+
+    var source = options.source;   // Source file path
+    var logo = options.logo;   // Logo file path
+    var ext = source.split('.').pop();
+    var destination = options.destination; // Destination file path
+
+    /**
+    * Left bottom: X = 10, Y = logoY
+    * Right bottom: X = logoX, Y = logoY
+    * Left Top: X = 10, Y = 10
+    * Right Top: X = logoX, Y = 10
+    */
+
+    var position = options.position;   //'right-bottom';
+    var type = options.type;    //'text';
+    var text = options.text ? options.text : '';
+    gm(source)
+        .size(function (err, size) {
+            console.log(err);
+            if (!err) {
+                var logoWidth = (size.width / 10);
+                var logoHeight = (size.height / 10);
+                var logoX = size.width - (size.width / 8);
+                var logoY = size.height - (size.height / 8);
+                console.log(logoWidth + '==' + logoHeight + "==" + logoX + "==" + logoY);
+                switch (position) {
+                    case 'left-top':
+                        var logoX = 10;
+                        var logoY = 10;
+                        break;
+
+                    case 'left-bottom':
+                        var logoX = 10;
+                        var logoY = size.height - (size.height / 8);
+                        break;
+
+                    case 'right-top':
+                        var logoX = size.width - (size.width / 8);
+                        var logoY = 10;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (type === 'text') {
+                    gm(source)
+                        .drawText(logoX, logoY, 'from scratch')
+                        .write(destination, function (e) {
+                            console.log(e || 'Text Watermark Done'); // What would you like to do here?
+                            if (!e) {
+                                callback({ status: 1 });
+                            } else {
+                                callback({ status: 0 });
+                            }
+                        });
+                } else {
+                    gm(source)
+                        .draw(['image over ' + logoX + ',' + logoY + ' ' + logoWidth + ',' + logoHeight + ' ' + logo])
+                        .write(destination, function (e) {
+                            console.log(e || 'Image Watermark Done'); // What would you like to do here?
+                            if (!e) {
+                                callback({ status: 1 });
+                            } else {
+                                callback({ status: 0 });
+                            }
+                        });
+                }
+            }
+        });
 };
 
 module.exports.embed = embed;
